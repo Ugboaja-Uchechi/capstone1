71c7e4a3b4d75b4fc22b0c81ddbaeb771225a959
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2021-09-16 20:46:26 +0100

    Fix mobile speakers section

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const speakers = [
     mainImage: 'images/emilia-clarke.jpg',
     topic: 'Emilia Clarke',
     occupation: 'English Actress',
-    description: 'Emilia Isobel Euphemia Rose Clarke is an English actress. She studied at the Drama Centre London, appearing in a number of stage productions. Her television debut was a guest appearance in the BBC One medical soap opera Doctors in 2009.',
+    description: 'Emilia Isobel Euphemia Rose Clarke is an English actress. She studied at the Drama Centre London. Lorem ipsum',
   },
   {
     id: 1,
@@ -30,7 +30,7 @@ const speakers = [
     mainImage: 'images/kit-harrington.jpg',
     topic: 'Kit Harrington',
     occupation: 'English Actor',
-    description: "Christopher Catesby 'Kit' Harington is an English actor. He studied at the Royal Central School of Speech & Drama and made his professional acting debut with the lead role of Albert Narracott in the critically acclaimed West End play War Horse at the National Theatre.",
+    description: "Christopher Catesby 'Kit' Harington is an English actor. He studied at the Royal Central School of Speech & Drama.",
   },
   {
     id: 2,
@@ -38,7 +38,7 @@ const speakers = [
     mainImage: 'images/maisie-wlliams.jpg',
     topic: 'Maisie Williams',
     occupation: 'English Actress',
-    description: "Margaret Constance 'Maisie' Williams is an English actress who made her acting debut in 2011 as Arya Stark, a lead character in the HBO epic medieval fantasy drama series Game of Thrones.",
+    description: "Margaret Constance 'Maisie' Williams is an English actress who made her acting debut in 2011 as Arya Stark.",
   },
   {
     id: 3,
@@ -46,7 +46,7 @@ const speakers = [
     mainImage: 'images/sophie-turner.jpg',
     topic: 'Sophie Jonas',
     occupation: 'English Actress',
-    description: 'Sophie Belinda Jonas is an English actress. She made her acting debut as Sansa Stark on the HBO epic fantasy television series Game of Thrones, for which she received a Primetime Emmy Award nomination for the Outstanding Supporting Actress in a Drama Series in 2019',
+    description: 'Sophie Belinda Jonas is an English actress. She made her acting debut as Sansa Stark on the H.B.O series G.O.T',
   },
   {
     id: 4,
@@ -54,7 +54,7 @@ const speakers = [
     mainImage: 'images/lena-headey.jpg',
     topic: 'Lena Headey',
     occupation: 'English Actress',
-    description: 'Lena Kathren Headey is an English actress. She gained international recognition and acclaim for her portrayal of Cersei Lannister on the HBO epic fantasy drama series Game of Thrones, for which she received five Primetime Emmy Award nominations and a Golden Globe Award nomination.',
+    description: 'Lena Kathren Headey is an English actress. She gained international recognition and acclaim for her portrayal.',
   },
   {
     id: 5,
@@ -62,7 +62,7 @@ const speakers = [
     mainImage: 'images/peter-dinklage.jpg',
     topic: 'Peter Dinklage',
     occupation: 'English Actress',
-    description: 'Peter Hayden Dinklage is an American actor and producer. He received acclaim for portraying Tyrion Lannister on the HBO television series Game of Thrones, for which he won the Primetime Emmy Award for Outstanding Supporting Actor in a Drama Series a record four times',
+    description: 'Peter Hayden Dinklage is an American actor and producer. He received acclaim for portraying Tyrion Lannister',
   },
 ];
 
